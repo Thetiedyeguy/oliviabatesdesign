5f6359a80c532a487c3723c1ff9b3da800b358d9
Thetiedyeguy <Thetiedyeguy42@gmail.com>
2025-04-15 16:18:44 -0700

    fixed for real this time

--- client/src/pages/Projects/Projects.jsx
+++ client/src/pages/Projects/Projects.jsx
@@ -39,6 +39,15 @@ const Projects = () => {
 
   return (
     <div className={styles.projectsHorizontalScroll}>
+      <div className={styles.beginningInfo}>
+        <p className={styles.intro}>This is work from my time at</p>
+        <h1 className={styles.intro}>University of California - Davis</h1>
+        <p className={styles.intro}>While persuing a B.A. in Design and a B.A. in Communications</p>
+        <div className={styles.timelineSection}>
+          <p className={styles.timeline}>Start Fall 2021</p>
+          <p className={styles.timeline}>End Winter 2025</p>
+        </div>
+      </div>
       {projects.map((project, index) => (
         <div className={styles.projectSlide} key={project.id}>
           {/* Each "slide" in the horizontal scroll */}
